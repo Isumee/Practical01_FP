d65e96893303f8a9e3da60ea1f4de21be9dd1750
Isumee <140165567+Isumee@users.noreply.github.com>
2024-06-18 01:38:40 +0530

    question04.scala

--- question04.scala
+++ question04.scala
@@ -2,6 +2,6 @@ package com.question04.scala
 
 object question04 {
   private def discountPrice(n:Double):Double = 24.95*0.4*n
-  private def shippingCost(n:Double):Double = if (n<=50) {3} else {3+75*(n-50)}
+  private def shippingCost(n:Double):Double = if (n<=50) {3*n} else {3*n+75*(n-50)}
   def wholesaleCost(n:Double):Double = shippingCost(n)+24.95*n-discountPrice(n)
 }
