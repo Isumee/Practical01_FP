2091fd3007799c3bf7d47179108d690f02592c98
Isumee <140165567+Isumee@users.noreply.github.com>
2024-06-18 07:35:30 +0530

    question01.scala

--- question01.scala
+++ question01.scala
@@ -2,4 +2,9 @@ package com.question01.scala
 
 object question01 {
    def areaOfDisk(r:Double): Double = math.Pi*r*r
+   def main(args: Array[String]): Unit = {
+      val radius = 5.0
+      val area = areaOfDisk(radius)
+      println(f"The area of disk is $area")
+   }
 }
