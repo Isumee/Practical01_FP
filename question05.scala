891d66b19191d3b926731eca18234b599dbdef66
Isumee <140165567+Isumee@users.noreply.github.com>
2024-06-18 07:48:19 +0530

    question05.scala

--- question05.scala
+++ question05.scala
@@ -4,4 +4,11 @@ object question05 {
   private def easyPace(l:Int):Int=l*8
   private def TempoPace(l:Int):Int=l*7
   def totalTime(l1:Int,l2:Int,l3:Int):Int=easyPace(l1)+TempoPace(l2)+easyPace(l3)
+  def main(args: Array[String]): Unit = {
+    val l1=2
+    val l2=3
+    val l3 =2
+    val totalTimeTaken = totalTime(l1,l2,l3)
+    println(f"Total running time is $totalTimeTaken")
+  }
 }
