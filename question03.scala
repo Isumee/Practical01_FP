8ed9ae8ba4792e9a15406c22549edb88c6827012
Isumee <140165567+Isumee@users.noreply.github.com>
2024-06-18 07:41:38 +0530

    question03.scala

--- question03.scala
+++ question03.scala
@@ -2,4 +2,9 @@ package com.question03.scala
 
 object question03 {
   def volumeOfSphere(r:Double):Double = 4/3*math.Pi*math.pow(r,3)
+  def main(args: Array[String]): Unit = {
+    val radius = 5.0
+    val volume = volumeOfSphere(radius)
+    println(f"The area of disk is $volume")
+  }
 }
