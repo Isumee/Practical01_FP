397e0335c159877abb8fb15d4e7d53e6f647112d
Isumee <140165567+Isumee@users.noreply.github.com>
2024-06-18 07:44:24 +0530

    question04.scala

--- question04.scala
+++ question04.scala
@@ -4,4 +4,9 @@ object question04 {
   private def discountPrice(n:Double):Double = 24.95*0.4*n
   private def shippingCost(n:Double):Double = if (n<=50) {3*n} else {3*n+75*(n-50)}
   def wholesaleCost(n:Double):Double = shippingCost(n)+24.95*n-discountPrice(n)
+  def main(args: Array[String]): Unit = {
+    val copies = 60
+    val finalPrice = wholesaleCost(copies)
+    println(f"The final price of the books is $finalPrice")
+  }
 }
