1bb414f6462e24bb438df655f846874828193e3d
Isumee <140165567+Isumee@users.noreply.github.com>
2024-06-18 07:48:55 +0530

    question02.scala

--- question02.scala
+++ question02.scala
@@ -2,7 +2,8 @@ package com.question02.scala
 
 object question02 {
   def convertTemperature(t:Double):Double=t*1.8000+32.00
-   def main(args: Array[String]): Unit = {
+   
+  def main(args: Array[String]): Unit = {
     val cel = 35.0
     val fahrenheit = convertTemperature(cel)
     println(f"Fahrenheit temperature is $fahrenheit")
